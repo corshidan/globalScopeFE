677303a9fe2e580317ce81edf9cf016399ad1c42
proencaf <83221947+proencaf@users.noreply.github.com>
2021-10-20 15:54:40 +0100

    filipe.js

---
+++ filipe.js
@@ -0,0 +1,91 @@
+const bootcamperTable = {
+  fullName: 'Filipe Proenca',
+  bootcamperId: 300,
+  frequency: 2,
+  createdAt: '09/04/2021',
+}
+
+const reflections = {
+  bootcamperId: 300,
+  createdAt: '09/04/2021',
+  reflection: "I can't believe, today was a good day!",
+  isAccessible: false,
+  topicsPicked: [
+    { topic: 'React', rating: 0 },
+    { topic: 'CSS', rating: 3 },
+    { topic: 'HTML', rating: 4 },
+  ],
+  confidenceRating: 5,
+  gratefulFor: 'health and wealth',
+  areaOfImprovement: 'Exercise more',
+  overallFeeling: 6,
+}
+
+const bootcamperTable = {
+  fullName: 'Filipe Proenca',
+  bootcamperId: 300,
+  frequency: 2,
+  createdAt: '10/04/2021',
+}
+
+const reflections = {
+  bootcamperId: 300,
+  createdAt: '10/04/2021',
+  reflection: 'The key is to make it',
+  isAccessible: false,
+  topicsPicked: [
+    { topic: 'React', rating: 1 },
+    { topic: 'TypeScript', rating: 2 },
+    { topic: 'Javascript', rating: 4 },
+  ],
+  confidenceRating: 5,
+  gratefulFor: 'relaxation',
+  areaOfImprovement: 'eat more donuts',
+  overallFeeling: 6,
+}
+
+const bootcamperTable = {
+  fullName: 'Filipe Proenca',
+  bootcamperId: 300,
+  frequency: 2,
+  createdAt: '11/04/2021',
+}
+
+const reflections = {
+  bootcamperId: 300,
+  createdAt: '11/04/2021',
+  reflection: 'Shake it off, tomorrow is a new day',
+  isAccessible: true,
+  topicsPicked: [
+    { topic: 'React', rating: 3 },
+    { topic: 'Tailwind', rating: 0 },
+    { topic: 'Framer', rating: 0 },
+  ],
+  confidenceRating: 5,
+  gratefulFor: 'self improvement',
+  areaOfImprovement: 'Better understand react',
+  overallFeeling: 5,
+}
+
+const bootcamperTable = {
+  fullName: 'Filipe Proenca',
+  bootcamperId: 300,
+  frequency: 2,
+  createdAt: '12/04/2021',
+}
+
+const reflections = {
+  bootcamperId: 300,
+  createdAt: '12/04/2021',
+  reflection: 'Patience is key... keep pushing',
+  isAccessible: true,
+  topicsPicked: [
+    { topic: 'React', rating: 4 },
+    { topic: 'AWS', rating: 3 },
+    { topic: 'Cypress', rating: 3 },
+  ],
+  confidenceRating: 5,
+  gratefulFor: 'friends',
+  areaOfImprovement: 'Be more patient',
+  overallFeeling: 4,
+}
