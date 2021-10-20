da86bc49dd2a93a0d5150f91cd04d17e9a46047f
ellyd88 <elly.d@hotmail.co.uk>
2021-10-20 15:48:40 +0100

    added reflection

---
+++ elly.js
@@ -0,0 +1,23 @@
+const bootcamperTable = {
+    fullName: "Elly Durrant",
+    bootcamperId: "1234",
+    frequency: "4",
+    courseStartDate: "26/07/21"
+}
+
+const reflections = {
+    bootcamperId: "1234",
+    createdAt: "26/07/21",
+    reflection: "Today was really interesting, researching node-postgres via the docs, and then using this to create a server that can make requests to a database. I can see the benefit of looking through the docs thoroughly, but I think the skill will be searching the docs efficiently to find the key parts of the package that will actually be of use, as we ended up spending too much time reading the docs and didn’t complete many tasks in time.",
+    isAccessible: true,
+    topicsPicked: [
+        {topic: "Express", rating: 2},
+        {topic: "Postgress", rating: 3},
+        {topic: "Heroku", rating: 3},
+        {topic: "Restful APIs", rating: 2}
+    ],
+    confidenceRating: 3,
+    gratefulFor: "My pair for being supportive",
+    areaOfImprovement: "finish off the express-postgress workshop",
+    overallFeeling: 6
+}
