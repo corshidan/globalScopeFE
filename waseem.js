7f4c7eba8de0fbdfb51bea1c99dcb3241e4ba6c1
Waseem <waseembenaras@gmail.com>
2021-10-20 16:09:34 +0100

    waseem

---
+++ waseem.js
@@ -0,0 +1,49 @@
+const bootcampers = {
+    fullName: "Waseem Benaras",
+    bootcamperId: 5678,
+    frequency: 2,
+    courseStartDate: "26/07/2021"
+}
+
+const reflections = [
+  { bootcamperId: 5678,
+    createdAt: "28/07/2021",
+    reflection: `Today we looked at objects, arrays and loops. Think of objects as a box of things while arrays are like an itemised list. Need to practice more. More comfortable with loops today.`,
+    isAccessible: false,
+    topicsPicked: [
+        {topic: "objects", rating: 2},
+        {topic: "arrays", rating: 3},
+        {topic: "loops", rating: 4},
+    ] ,
+    confidenceRating: 3,
+    gratefulFor: "pair programming",
+    areaOfImprovement: "objects and arrays" ,
+    overallFeeling: 4
+},
+{ bootcamperId: 5678,
+    createdAt: "30/07/2021",
+    reflection: `Today was our first hackathon. It was challenging to get the task completed in time. Also, we worked in a group of three which was a bit different to before. Was good to try and build something even though we didn't complete it`,
+    isAccessible: true,
+    topicsPicked: [
+        {topic: "DOM", rating: 3},
+        
+    ] ,
+    confidenceRating: 3,
+    gratefulFor: "support from coaches",
+    areaOfImprovement: "planning" ,
+    overallFeeling: 3
+},
+{ bootcamperId: 5678,
+    createdAt: "05/08/2021",
+    reflection: `We looked at asychronous code which was interesting. I think I've got an understadning of how things move through the call stack `,
+    isAccessible: true,
+    topicsPicked: [
+        {topic: "async code", rating: 4},
+        
+    ] ,
+    confidenceRating: 3,
+    gratefulFor: "energisers",
+    areaOfImprovement: "just need to practice",
+    overallFeeling: 4
+}
+]
