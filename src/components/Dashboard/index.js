9247402d37149308f06da710e4867f2546f7eec1
proencaf <83221947+proencaf@users.noreply.github.com>
2021-10-22 16:05:19 +0100

    dashboard cards displaying ok

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,55 +1,55 @@
-import "./Dashboard.css";
+import './Dashboard.css'
 
 export default function Dashboard() {
   return (
-    <section className="blog-box">
-      <div class="container">
-        <div class="testimonial-card1">
-          <div class="text"></div>
-          <div class="footer">
-            <div class="image"></div>
-            <a href="/bloginputpage">
-              <h3 class="person">Add to your blog</h3>
+    <body>
+      <div class='container'>
+        <div class='testimonial-card1'>
+          <div class='text'></div>
+          <div class='footer'>
+            <div class='image'></div>
+            <a href='/bloginputpage'>
+              <h3 class='person'>Add to your blog</h3>
             </a>
           </div>
         </div>
       </div>
 
-      <div class="container">
-        <div class="testimonial-card2">
-          <div class="text"></div>
-          <div class="footer">
-            <div class="image"></div>
-            <a href="/blogrecappage">
-              <h3 class="person">Blog Recap</h3>
+      <div class='container'>
+        <div class='testimonial-card2'>
+          <div class='text'></div>
+          <div class='footer'>
+            <div class='image'></div>
+            <a href='/blogrecappage'>
+              <h3 class='person'>Blog Recap</h3>
             </a>
           </div>
         </div>
       </div>
 
-      <div class="container">
-        <div class="testimonial-card3">
-          <div class="text"></div>
-          <div class="footer">
-            <div class="image"></div>
-            <a href="/statspage">
-              <h3 class="person">See your stats</h3>
+      <div class='container'>
+        <div class='testimonial-card3'>
+          <div class='text'></div>
+          <div class='footer'>
+            <div class='image'></div>
+            <a href='/statspage'>
+              <h3 class='person'>See your stats</h3>
             </a>
           </div>
         </div>
       </div>
 
-      <div class="container">
-        <div class="testimonial-card4">
-          <div class="text"></div>
-          <div class="footer">
-            <div class="image"></div>
-            <a href="/listpage">
-              <h3 class="person">To do list</h3>
+      <div class='container'>
+        <div class='testimonial-card4'>
+          <div class='text'></div>
+          <div class='footer'>
+            <div class='image'></div>
+            <a href='/listpage'>
+              <h3 class='person'>To do list</h3>
             </a>
           </div>
         </div>
       </div>
-    </section>
-  );
+    </body>
+  )
 }
