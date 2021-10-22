2048e041a4ed6b96acc1db7f1a8603e9a591cb79
ellyd88 <elly.d@hotmail.co.uk>
2021-10-22 14:01:52 +0100

    added some input types

--- src/components/BlogInputPage/index.js
+++ src/components/BlogInputPage/index.js
@@ -1,7 +1,64 @@
-
+import "./BlogInputPage.css";
 
 export default function BlogInputPage() {
-    return (
-        <input type="text" placeholder="Add to blog"></input>
-    )
+  return (
+    <section>
+      <p>
+        What have you covered since last time? Rate your confidence for each
+        from 1-5:
+      </p>
+      <div className="topic-container">
+        <input type="text" placeholder="topic1"></input>
+        <form>
+          <label for="quantity"></label>
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            min="1"
+            max="5"
+          ></input>
+          <input type="submit" value="Submit"></input>
+        </form>
+        <input type="text" placeholder="topic2"></input>
+        <form>
+          <label for="quantity"></label>
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            min="1"
+            max="5"
+          ></input>
+          <input type="submit" value="Submit"></input>
+        </form>
+        <input type="text" placeholder="topic3"></input>
+        <form>
+          <label for="quantity"></label>
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            min="1"
+            max="5"
+          ></input>
+          <input type="submit" value="Submit"></input>
+        </form>
+        <input type="text" placeholder="topic4"></input>
+        <form>
+          <label for="quantity"></label>
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            min="1"
+            max="5"
+          ></input>
+          <input type="submit" value="Submit"></input>
+        </form>
+      </div>
+      <p>Space for thoughts:</p>
+      <input type="text" placeholder="Add to blog"></input>
+    </section>
+  );
 }
