8b20e355ecacc63cbdacc8a0c450be6f8ef33e96
ellyd88 <elly.d@hotmail.co.uk>
2021-10-22 14:24:41 +0100

    more input types

--- src/components/BlogInputPage/index.js
+++ src/components/BlogInputPage/index.js
@@ -8,7 +8,7 @@ export default function BlogInputPage() {
         from 1-5:
       </p>
       <div className="topic-container">
-        <input type="text" placeholder="topic1"></input>
+        <input type="text" placeholder="topic1" />
         <form>
           <label for="quantity"></label>
           <input
@@ -18,9 +18,9 @@ export default function BlogInputPage() {
             min="1"
             max="5"
           ></input>
-          <input type="submit" value="Submit"></input>
+          <input type="submit" value="Submit" />
         </form>
-        <input type="text" placeholder="topic2"></input>
+        <input type="text" placeholder="topic2" />
         <form>
           <label for="quantity"></label>
           <input
@@ -30,9 +30,9 @@ export default function BlogInputPage() {
             min="1"
             max="5"
           ></input>
-          <input type="submit" value="Submit"></input>
+          <input type="submit" value="Submit" />
         </form>
-        <input type="text" placeholder="topic3"></input>
+        <input type="text" placeholder="topic3" />
         <form>
           <label for="quantity"></label>
           <input
@@ -42,9 +42,9 @@ export default function BlogInputPage() {
             min="1"
             max="5"
           ></input>
-          <input type="submit" value="Submit"></input>
+          <input type="submit" value="Submit" />
         </form>
-        <input type="text" placeholder="topic4"></input>
+        <input type="text" placeholder="topic4" />
         <form>
           <label for="quantity"></label>
           <input
@@ -54,11 +54,20 @@ export default function BlogInputPage() {
             min="1"
             max="5"
           ></input>
-          <input type="submit" value="Submit"></input>
+          <input type="submit" value="Submit" />
         </form>
       </div>
+      <p>
+        How confident do you feel about what you learned since last reflection?
+      </p>
+      <form>
+        <p>1 2 3 4 5</p>
+        <input type="range" min="0" max="5" step="1" />
+      </form>
+      <p>What do you want to work on/ what did not go well?</p>
+      <input type="text" placeholder="To work on" />
       <p>Space for thoughts:</p>
-      <input type="text" placeholder="Add to blog"></input>
+      <input type="text" placeholder="Add to blog" />
     </section>
   );
 }
