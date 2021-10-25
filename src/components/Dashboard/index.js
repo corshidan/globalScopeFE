6264ea3b1ca68b0c3f314a3db7bb8da9f0b281c7
ellyd88 <elly.d@hotmail.co.uk>
2021-10-25 11:04:23 +0100

    added cards with working paths

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,63 +1,36 @@
-import './Dashboard.css';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Sidebar from '../Sidebar';
-import Card from '../Card';
+import "./Dashboard.css";
+import React from "react";
+import Sidebar from "../Sidebar";
+import Card from "../Card";
 
 export default function Dashboard() {
-	return (
-		<div>
-			<Sidebar />
-			<Card />
-			<body>
-				<div class="container">
-					<div class="testimonial-card1">
-						<div class="text"></div>
-						<div class="footer">
-							<div class="image"></div>
-							<a href="/bloginputpage">
-								<h3 class="person">Add to your blog</h3>
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="container">
-					<div class="testimonial-card2">
-						<div class="text"></div>
-						<div class="footer">
-							<div class="image"></div>
-							<a href="/blogrecappage">
-								<h3 class="person">Blog Recap</h3>
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="container">
-					<div class="testimonial-card3">
-						<div class="text"></div>
-						<div class="footer">
-							<div class="image"></div>
-							<a href="/statspage">
-								<h3 class="person">See your stats</h3>
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="container">
-					<div class="testimonial-card4">
-						<div class="text"></div>
-						<div class="footer">
-							<div class="image"></div>
-							<a href="/listpage">
-								<h3 class="person">To do list</h3>
-							</a>
-						</div>
-					</div>
-				</div>
-			</body>
-		</div>
-	);
+  return (
+    <div>
+      <Sidebar />
+      <Card
+        image="/images/addToBlogImage.png"
+        title="Add to Blog"
+        buttonLabel="Add"
+        path="/bloginputpage"
+      />
+      <Card
+        image="/images/seeYourStatsImage.png"
+        title="See your Stats"
+        buttonLabel="Stats"
+        path=""
+      />
+      <Card
+        image="/images/recapBlogImage.png"
+        title="Blog Recap"
+        buttonLabel="Recap"
+        path="/blogrecappage"
+      />
+      <Card
+        image="/images/toDoListImage.png"
+        title="To do List"
+        buttonLabel="Add"
+        path=""
+      />
+    </div>
+  );
 }
