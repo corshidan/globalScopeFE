41d43ee5f602340374fdf856228d0d952a2ec9e2
Waseem <waseembenaras@gmail.com>
2021-10-25 16:04:09 +0100

    input page stuff

--- src/components/BlogInputPage/index.js
+++ src/components/BlogInputPage/index.js
@@ -1,49 +1,125 @@
-import './BlogInputPage.css';
-import Layout from '../Layout';
+import css from "./bloginputpage.module.css";
+import Layout from "../Layout";
 
 export default function BlogInputPage() {
-	return (
-		<Layout>
-			<section>
-				<p>
-					What have you covered since last time? Rate your confidence for each from 1-5:
-				</p>
-				<div className="topic-container">
-					<input type="text" placeholder="topic1" />
-					<form>
-						<label for="quantity"></label>
-						<input type="number" id="quantity" name="quantity" min="1" max="5"></input>
-						<input type="submit" value="Submit" />
-					</form>
-					<input type="text" placeholder="topic2" />
-					<form>
-						<label for="quantity"></label>
-						<input type="number" id="quantity" name="quantity" min="1" max="5"></input>
-						<input type="submit" value="Submit" />
-					</form>
-					<input type="text" placeholder="topic3" />
-					<form>
-						<label for="quantity"></label>
-						<input type="number" id="quantity" name="quantity" min="1" max="5"></input>
-						<input type="submit" value="Submit" />
-					</form>
-					<input type="text" placeholder="topic4" />
-					<form>
-						<label for="quantity"></label>
-						<input type="number" id="quantity" name="quantity" min="1" max="5"></input>
-						<input type="submit" value="Submit" />
-					</form>
-				</div>
-				<p>How confident do you feel about what you learned since last reflection?</p>
-				<form>
-					<p>1 2 3 4 5</p>
-					<input type="range" min="0" max="5" step="1" />
-				</form>
-				<p>What do you want to work on/ what did not go well?</p>
-				<input type="text" placeholder="To work on" />
-				<p>Space for thoughts:</p>
-				<input type="text" placeholder="Add to blog" />
-			</section>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <form className="border-2">
+        <div className="flex flex-col border-4">
+          <label for="topics-section">
+            What topics have you covered since you were last here? Rate your
+            confidence for each from 1-5:
+          </label>
+          <div className="flex">
+            <label for="topicOne"></label>
+            <input
+              id="topicOne"
+              name="topicOne"
+              list="topics"
+              placeholder="The first topic was..."
+            />
+            <label for="ratingTopicOne"></label>
+            <input
+              type="number"
+              id="ratingTopicOne"
+              name="ratingTopicOne"
+              min="1"
+              max="5"
+            ></input>
+          </div>
+	<datalist id="topics">
+		<option value="JS"></option>
+	</datalist>
+		<div className="flex">
+		<label for="topicTwo"></label>
+		  <input 
+		  	id="topicTwo"
+            name="topicTwo"
+			list="topics" 
+			placeholder="And..."
+		  />
+          <label for="ratingTopicTwo"></label>
+          <input
+            type="number"
+            id="ratingTopicTwo"
+            name="ratingTopicTwo"
+            min="1"
+            max="5"
+          ></input>
+		</div>
+
+          <div className="flex">
+		<label for="topicThree"></label>
+		  <input 
+		  	id="topicThree"
+            name="topicThree"
+			list="topics" 
+			placeholder="Also..."
+		  />
+          <label for="ratingTopicThree"></label>
+          <input
+            type="number"
+            id="ratingTopicThree"
+            name="ratingTopicThree"
+            min="1"
+            max="5"
+          ></input>
+		</div>
+
+		<div className="flex">
+		<label for="topicFour"></label>
+		  <input 
+		  	id="topicFour"
+            name="topicFour"
+			list="topics" 
+			placeholder="Wait, I nearly forgot..."
+		  />
+          <label for="ratingTopicFour"></label>
+          <input
+            type="number"
+            id="ratingTopicFour"
+            name="ratingTopicFour"
+            min="1"
+            max="5"
+          ></input>
+		</div>
+        </div>
+
+        <div className="flex flex-col border-4">
+          <label for="confidence-range">
+            How confident do you feel about what you've learned since your last
+            reflection?
+          </label>
+          <p>1 2 3 4 5</p>
+          <input id="confidence-range" type="range" min="0" max="5" step="1" />
+        </div>
+
+        <div className="flex flex-col border-4">
+          <label for="improvements">
+            What could have gone better / What areas would you like to improve?
+          </label>
+          <textarea
+		  className="border-2"
+            id="improvements"
+            name="improvements"
+            placeholder="Enter your thoughts here..."
+            rows="6"
+            cols="50"
+          ></textarea>
+        </div>
+
+        <div className="flex flex-col border-4">
+          <label for="add-thoughts">Any other thoughts?:</label>
+          <textarea
+		  	className="border-2"
+            id="add-thoughts"
+            name="add-thoughts"
+            placeholder="I've run out of peanut butter..."
+            rows="6"
+            cols="50"
+          />
+        </div>
+      </form>
+    </Layout>
+  );
 }
