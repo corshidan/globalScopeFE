f077562010bbdaa6c30420ce9462917ef3e4524a
proencaf <83221947+proencaf@users.noreply.github.com>
2021-10-25 11:15:32 +0100

    layout card update

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Card({ image, title, buttonLabel, path }) {
   return (
-    <div className="max-w-lg mx-auto">
-      <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm mb-5">
-        <img className="rounded-t-lg" src={image} alt="" />
+    <div className='max-w-lg mx-auto inline-flex'>
+      <div className='bg-white shadow-md border border-gray-200 rounded-lg max-w-sm mb-5 '>
+        <img className='rounded-t-lg' src={image} alt='' />
 
-        <div className="p-5">
-          <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">
+        <div className='p-5'>
+          <h5 className='text-gray-900 font-bold text-2xl tracking-tight mb-2'>
             {title}
           </h5>
 
           <Link
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center"
+            className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center'
             to={path}
           >
             {buttonLabel}
@@ -21,5 +21,5 @@ export default function Card({ image, title, buttonLabel, path }) {
         </div>
       </div>
     </div>
-  );
+  )
 }
