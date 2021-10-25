d8365d76caf090ca255ea0dad1eefb70664308dc
proencaf <83221947+proencaf@users.noreply.github.com>
2021-10-25 15:15:26 +0100

    header date working

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,11 @@
 import './Header.css'
 
 export default function Header() {
+  const current = new Date()
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`
+
   return (
     <>
       <header className='header'>
@@ -10,6 +15,7 @@ export default function Header() {
           "Only compare yourself if you know you’re better than the other
           person"
         </p>
+        <p className='date'> {date}</p>
       </header>
     </>
   )
