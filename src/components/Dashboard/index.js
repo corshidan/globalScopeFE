7a05774eec533be731274ebd0646329f558838b0
corshi.dan <corshi.dan@gmail.com>
2021-10-25 13:46:09 +0100

    _

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -6,25 +6,32 @@ import Card from '../Card';
 export default function Dashboard() {
 	return (
 		<Layout>
-			<Card
-				image="/images/addToBlogImage.png"
-				title="Add to Blog"
-				buttonLabel="Add"
-				path="/bloginputpage"
-			/>
-			<Card
-				image="/images/seeYourStatsImage.png"
-				title="See your Stats"
-				buttonLabel="Stats"
-				path=""
-			/>
-			<Card
-				image="/images/recapBlogImage.png"
-				title="Blog Recap"
-				buttonLabel="Recap"
-				path="/blogrecappage"
-			/>
-			<Card image="/images/toDoListImage.png" title="To do List" buttonLabel="Add" path="" />
+			<div className="flex justify-between h-full items-center">
+				<Card
+					image="/images/addToBlogImage.png"
+					title="Add to Blog"
+					buttonLabel="Add"
+					path="/bloginputpage"
+				/>
+				<Card
+					image="/images/seeYourStatsImage.png"
+					title="See your Stats"
+					buttonLabel="Stats"
+					path=""
+				/>
+				<Card
+					image="/images/recapBlogImage.png"
+					title="Blog Recap"
+					buttonLabel="Recap"
+					path="/blogrecappage"
+				/>
+				<Card
+					image="/images/toDoListImage.png"
+					title="To do List"
+					buttonLabel="Add"
+					path=""
+				/>
+			</div>
 		</Layout>
 	);
 }
