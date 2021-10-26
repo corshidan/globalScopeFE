5d22db959f7243aed61b06771e7b6516c600c83b
corshi.dan <corshi.dan@gmail.com>
2021-10-26 17:40:26 +0100

    _

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -6,30 +6,36 @@ import css from './sidebar.module.css';
 const styles =
 	'flex  flex-col justify-center items-center mb-7 font-bold break-all text-center text-xl';
 export default function Sidebar() {
-  return (
-    <div className={css.sidebar}>
-      <div className={styles}>
-        <p>Bootcamper</p>
-        <p>Dashboard</p>
-      </div>
+	return (
+		<div className={css.sidebar}>
+			<div className={styles}>
+				<p>Bootcamper</p>
+				<p>Dashboard</p>
+			</div>
 
 			<ul className={css.navigation}>
 				<li className="rounded-l-full hover:bg-green-400 hover:text-white duration-300">
 					<Link to="/dashboard">
-						<FontAwesomeIcon size="sm" icon={faChartLine} />
-						<span className="ml-2">Dashboard</span>
+						<p className="">
+							<FontAwesomeIcon size="sm" icon={faChartLine} />
+							<span className="ml-2">Dashboard</span>
+						</p>
 					</Link>
 				</li>
 				<li className="rounded-l-full hover:bg-green-400 hover:text-white duration-300">
 					<Link to="/bloginputpage">
-						<FontAwesomeIcon size="sm" icon={faBrain} />
-						<span className="ml-2">Add Reflection</span>
+						<p>
+							<FontAwesomeIcon size="sm" icon={faBrain} />
+							<span className="ml-2">Add Reflection</span>
+						</p>
 					</Link>
 				</li>
 				<li className="rounded-l-full hover:bg-green-400 hover:text-white duration-300">
 					<Link to="/blogrecappage">
-						<FontAwesomeIcon size="sm" icon={faBinoculars} />
-						<span className="ml-2">Review Reflections</span>
+						<p>
+							<FontAwesomeIcon size="sm" icon={faBinoculars} />
+							<span className="ml-2">Review Reflections</span>
+						</p>
 					</Link>
 				</li>
 			</ul>
