21b246b2979a458bb5e5340255bc3cfd3b0c1ef5
proencaf <83221947+proencaf@users.noreply.github.com>
2021-10-26 14:01:19 +0100

    sidebar animation

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import css from './sidebar.module.css';
-const styles = 'mb-7 font-bold break-all text-center text-xl';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import css from './sidebar.module.css'
+const styles = 'mb-7 font-bold break-all text-center text-xl'
 export default function Sidebar() {
-	return (
-		<div className={css.sidebar}>
-			<div className={styles}>
-				<p>Bootcamper</p>
-				<p>Dashboard</p>
-			</div>
-
-			<Link to="/dashboard">Dashboard</Link>
-			<Link to="/bloginputpage">Blog Input Page</Link>
-			<Link to="/blogrecappage">Blog Recap Page</Link>
-		</div>
-	);
+  return (
+    <div className={css.sidebar}>
+      <div className={styles}>
+        <p>Bootcamper</p>
+        <p>Dashboard</p>
+      </div>
+      <ul>
+        <Link to='/dashboard'>Dashboard</Link>
+        <Link to='/bloginputpage'>Blog Input Page</Link>
+        <Link to='/blogrecappage'>Blog Recap Page</Link>
+      </ul>
+    </div>
+  )
 }
