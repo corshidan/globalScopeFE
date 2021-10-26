7dcd26bba7010bd992d3e2e0a94e93709168021d
corshi.dan <corshi.dan@gmail.com>
2021-10-26 10:25:05 +0100

    _

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Card({ image, title, buttonLabel, path }) {
-  return (
-    <div className='justify-center items-center'>
-      <div className='bg-white shadow-md border border-gray-200 rounded-lg  mb-5 w-3/5'>
-        <img className='rounded-t-lg' src={image} alt='' />
+	return (
+		<div className=" flex justify-center items-center">
+			<div className="flex flex-col items-center bg-white shadow-md border border-gray-200 rounded-lg   w-1/3 ">
+				<img className="rounded-t-lg w-3/4" src={image} alt="" />
 
-        <div className='p-5'>
-          <h5 className='text-gray-900 font-bold text-2xl tracking-tight mb-2'>
-            {title}
-          </h5>
+				<div className="flex flex-col items-center p-5">
+					<h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">
+						{title}
+					</h5>
 
-          <Link
-            className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center'
-            to={path}
-          >
-            {buttonLabel}
-          </Link>
-        </div>
-      </div>
-    </div>
-  )
+					<Link
+						className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center"
+						to={path}
+					>
+						{buttonLabel}
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,39 +1,44 @@
-import './Dashboard.css'
-import React from 'react'
-import Layout from '../Layout'
-import Card from '../Card'
+import './Dashboard.css';
+import React from 'react';
+import Layout from '../Layout';
+import Card from '../Card';
 
-//flex 
+//flex
 
 export default function Dashboard() {
-  return (
-    <Layout>
-      <div className='grid grid-cols-2 p-40'>
-        <Card
-          image='/images/addToBlogImage.png'
-          title='Add to Blog'
-          buttonLabel='Add'
-          path='/bloginputpage'
-        />
-        <Card
-          image='/images/seeYourStatsImage.png'
-          title='See your Stats'
-          buttonLabel='Stats'
-          path=''
-        />
-        <Card
-          image='/images/recapBlogImage.png'
-          title='Blog Recap'
-          buttonLabel='Recap'
-          path='/blogrecappage'
-        />
-        <Card
-          image='/images/toDoListImage.png'
-          title='To do List'
-          buttonLabel='Add'
-          path=''
-        />
-      </div>
-    </Layout>
-  )
+	return (
+		<Layout>
+			{/* <div className="grid grid-cols-2 p-10"> */}
+			<div className="flex flex-col justify-center items-center">
+				<div className="flex justify-center items-center ">
+					<Card
+						image="/images/addToBlogImage.png"
+						title="Add to Blog"
+						buttonLabel="Add"
+						path="/bloginputpage"
+					/>
+					<Card
+						image="/images/seeYourStatsImage.png"
+						title="See your Stats"
+						buttonLabel="Stats"
+						path=""
+					/>
+				</div>
+				<div className="flex justify-center items-center ">
+					<Card
+						image="/images/recapBlogImage.png"
+						title="Blog Recap"
+						buttonLabel="Recap"
+						path="/blogrecappage"
+					/>
+					<Card
+						image="/images/toDoListImage.png"
+						title="To do List"
+						buttonLabel="Add"
+						path=""
+					/>
+				</div>
+			</div>
+		</Layout>
+	);
 }
