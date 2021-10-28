56de7baf797ba0a95ad2c55f84be760c3a0888b5
corshi.dan <corshi.dan@gmail.com>
2021-10-28 17:08:18 +0100

    _

--- src/components/BlogRecapPage/index.js
+++ src/components/BlogRecapPage/index.js
@@ -1,33 +1,41 @@
 import Layout from '../Layout';
 // import { useUser } from '../App/App.js';
+import { useState } from 'react';
 import css from './index.module.css';
 
-const reflections = [
-	{
-		bootcamperid: 10,
-		reflection:
-			'Really enjoyed the day, the vibe is good, high morale, we definitely need to work a bit more on the design of the project.    It feels like there is so much more to work on , maybe a bit overwhelming but slowly and with perseverance we keep going forward',
-		accessible: true,
-		topics: [
-			{ topic: 'primitive types', rating: 4 },
-			{ topic: 'complex types', rating: 2 },
-			{ topic: 'pass by reference', rating: 3 },
-			{ topic: 'cognitive load', rating: 5 },
-		],
-		confidence: 4,
-		grateful: 'how awesome this course is',
-		improvements: 'complex types, computational thinking',
-		overallFeeling: 5,
-	},
-];
-
+// const reflections = [
+// 	{
+// 		bootcamperid: 10,
+// 		reflection:
+// 			'Really enjoyed the day, the vibe is good, high morale, we definitely need to work a bit more on the design of the project.    It feels like there is so much more to work on , maybe a bit overwhelming but slowly and with perseverance we keep going forward',
+// 		accessible: true,
+// 		topics: [
+// 			{ topic: 'primitive types', rating: 4 },
+// 			{ topic: 'complex types', rating: 2 },
+// 			{ topic: 'pass by reference', rating: 3 },
+// 			{ topic: 'cognitive load', rating: 5 },
+// 		],
+// 		confidence: 4,
+// 		grateful: 'how awesome this course is',
+// 		improvements: 'complex types, computational thinking',
+// 		overallFeeling: 5,
+// 	},
+// ];
 
 export default function BlogRecapPage() {
 	// const user = useUser();
 	const date = new Date();
+	const [reflections, setReflections] = useState(null);
+	const [reflectionDate, setReflectionDate] = useState(null);
 	const today = date.toISOString().slice(0, 10);
-	let face = `/images/emojis/${reflections[0].overallFeeling}.png` || "/images/emojis/3.png";
-
+	// let face = `/images/emojis/${reflections[0].overallFeeling}.png` || '/images/emojis/3.png';
+	const getReflection = (date, id, e) => {
+		e.preventDefault();
+		console.log(date, id);
+		fetch(`https://global-scope.herokuapp.com/reflections?date=${date}&id=${id}`)
+			.then((res) => res.json())
+			.then((response) => setReflections([...response.payload]));
+	};
 	return (
 		<Layout>
 			<div
@@ -35,8 +43,14 @@ export default function BlogRecapPage() {
 				style={{ backgroundImage: 'url(/images/recapBlogImage.png)' }}
 			>
 				<form className="justify-center items-center  px-4  flex flex-col  pb-3 shadow-lg  opacity-95  h-full rounded-3xl ">
-					<div className={`${"bg-white px-6 py-4 my-6 rounded-2xl border-4 border-green-200 shadow-inner h-full m-2"} ${css.capital}`}>
-						<label className="mr-3" htmlFor="start">What day do you want to review? </label>
+					<div
+						className={`${'bg-white px-6 py-4 my-6 rounded-2xl border-4 border-green-200 shadow-inner h-full m-2'} ${
+							css.capital
+						}`}
+					>
+						<label className="mr-3" htmlFor="start">
+							What day do you want to review?{' '}
+						</label>
 						<input
 							className="rounded text-center border border-green-400 shadow-lg focus:ring-1 ring-blue-400 "
 							type="date"
@@ -45,45 +59,61 @@ export default function BlogRecapPage() {
 							name="reflection-date"
 							min="2021-01-01"
 							max="2021-12-31"
+							onChange={(e) => setReflectionDate(e.target.value)}
 						/>
-						<button className="btn btn-sm btn-accent shadow-xl bg-green-400 mb-5 ml-5 ">
+						<button
+							className="btn btn-sm btn-accent shadow-xl bg-green-400 mb-5 ml-5 "
+							onClick={(e) => getReflection(reflectionDate, '3', e)}
+						>
 							Look up
 						</button>
 						<br />
-						<label htmlFor="topics-section">
-							Topics you have covered on Week 4 day 2, and your confidence rating out
-							of 5:
-						</label>
-						<ul className="flex textarea h-20 textarea-bordered textarea-accent justify-around font-bold mt-2 mb-4 ">
-							{reflections[0].topics.map((topic, i) => {
-								return (
-									<li key={i}>
-										{topic.topic} - <span className="text-lg">{topic.rating}</span>
-									</li>
-								);
-							})}
-						</ul>
-						<label htmlFor="confidence-range">
-							How confident did you feel about what you've learned since your last
-							reflection?
-						</label>
-						<p className=" textarea h-5 font-bold textarea-accent mt-2 mb-4  ">4/5</p>
-						<label htmlFor="improvements">What you needed to work on...</label>
-						<p className="improvement textarea h-20 textarea-bordered textarea-accent font-bold mt-2 mb-4  ">
-							{reflections[0].improvements}
-						</p>
-						<label htmlFor="add-thoughts">Other thoughts...</label>
-						<p className="reflection textarea textarea-bordered textarea-accent font-bold mt-2 mb-4  ">
-							{reflections[0].reflection}
-						</p>
-						<label htmlFor="feeling-score">How you were feeling:</label>
-						<p className="reflection textarea textarea-bordered textarea-accent font-bold mt-2 mb-4  ">
-						<img src={face} alt="emoji face"/>
-						</p>
-						<label htmlFor="feeling-score">What you were grateful for:</label>
-						<p className="reflection textarea textarea-bordered textarea-accent font-bold mt-2 mb-4  ">
-							{reflections[0].grateful}
-						</p>
+						{reflections ? (
+							<div>
+								<label htmlFor="topics-section">
+									Topics you have covered on Week 4 day 2, and your confidence
+									rating out of 5:
+								</label>
+								<ul className="flex textarea h-20 textarea-bordered textarea-accent justify-around font-bold mt-2 mb-4 ">
+									{reflections[0].topics.map((topic, i) => {
+										return (
+											<li key={i}>
+												{topic.topic} -{' '}
+												<span className="text-lg">{topic.rating}</span>
+											</li>
+										);
+									})}
+								</ul>
+								<label htmlFor="confidence-range">
+									How confident did you feel about what you've learned since your
+									last reflection?
+								</label>
+								<p className=" textarea h-5 font-bold textarea-accent mt-2 mb-4  ">
+									4/5
+								</p>
+								<label htmlFor="improvements">What you needed to work on...</label>
+								<p className="improvement textarea h-20 textarea-bordered textarea-accent font-bold mt-2 mb-4  ">
+									{reflections[0].improvements}
+								</p>
+								<label htmlFor="add-thoughts">Other thoughts...</label>
+								<p className="reflection textarea textarea-bordered textarea-accent font-bold mt-2 mb-4  ">
+									{reflections[0].reflection}
+								</p>
+								<label htmlFor="feeling-score">How you were feeling:</label>
+								<p className="reflection textarea textarea-bordered textarea-accent font-bold mt-2 mb-4  ">
+									<img
+										src={`/images/emojis/${reflections[0].overallfeeling}.png`}
+										alt="emoji face"
+									/>
+								</p>
+								<label htmlFor="feeling-score">What you were grateful for:</label>
+								<p className="reflection textarea textarea-bordered textarea-accent font-bold mt-2 mb-4  ">
+									{reflections[0].grateful}
+								</p>
+							</div>
+						) : (
+							''
+						)}
 					</div>
 				</form>
 			</div>
