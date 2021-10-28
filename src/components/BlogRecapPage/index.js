9145ca6a22c7ee5739299d653f9e3607b48ad792
proencaf <83221947+proencaf@users.noreply.github.com>
2021-10-28 10:12:18 +0100

    recap page some styling fixes

--- src/components/BlogRecapPage/index.js
+++ src/components/BlogRecapPage/index.js
@@ -31,26 +31,27 @@ export default function BlogRecapPage() {
         }`}
         style={{ backgroundImage: 'url(/images/recapBlogImage.png)' }}
       >
-        <form className='justify-center items-center bg-white px-2 flex flex-col  bg-green-500 shadow-lg  opacity-95  h-full rounded-3xl '>
-          <div className='border-2 bg-white  p-3 pt-10 pb-4 rounded-2xl shadow-inner capitalize '>
+        <form className='justify-center items-center  px-4  flex flex-col  bg-green-500 shadow-lg  opacity-95  h-full rounded-3xl '>
+          <div className='border-1 bg-white px-6 py-10 my-6 rounded-2xl shadow-inner capitalize h-full m-2'>
             <label htmlFor='start'>What day do you want to review? </label>
             <input
               className='border-2 border-green-400 '
               type='date'
+              value='2021-10-25'
               id='input'
               name='reflection-date'
               min='2021-01-01'
               max='2021-12-31'
             />
             <button className='btn btn-sm btn-accent shadow-xl bg-green-400 mb-5 bg-opacity-100  ml-5  '>
-              Search
+              Look up
             </button>
             <br />
             <label htmlFor='topics-section'>
               Topics you have covered on Week 4 day 2, and your confidence
               rating out of 5:
             </label>
-            <ul className='flex textarea h-20 textarea-bordered textarea-accent justify-around font-extrabold mt-2 mb-2 '>
+            <ul className='flex textarea h-20 textarea-bordered textarea-accent justify-around font-bold mt-4 mb-4 '>
               {reflections[0].topics.map((topic, i) => {
                 return (
                   <li key={i}>
@@ -63,16 +64,16 @@ export default function BlogRecapPage() {
               How confident did you feel about what you've learned since your
               last reflection?
             </label>
-            <p className=' textarea h-5 font-extrabold textarea-accent mt-2 mb-2  '>
+            <p className=' textarea h-5 font-bold textarea-accent mt-4 mb-4  '>
               4/5
             </p>
             <label htmlFor='improvements'>What you needed to work on...</label>
-            <p className='improvement textarea h-20 textarea-bordered textarea-accent font-extrabold mt-2 mb-2   '>
+            <p className='improvement textarea h-20 textarea-bordered textarea-accent font-bold mt-4 mb-4  '>
               {reflections[0].improvements}
             </p>
 
             <label htmlFor='add-thoughts'>Other thoughts...</label>
-            <p className='reflection textarea h-20 textarea-bordered textarea-accent font-extrabold mt-2 mb-2   '>
+            <p className='reflection textarea h-20 textarea-bordered textarea-accent font-bold mt-4 mb-4  '>
               {reflections[0].reflection}
             </p>
             <label htmlFor='feeling-score'>How you were feeling?</label>
