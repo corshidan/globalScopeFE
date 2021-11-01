e6cf0e8e1d4efe5d3a686ef89d6588edb9b36f1c
proencaf <83221947+proencaf@users.noreply.github.com>
2021-11-01 16:47:20 +0000

    updates

--- src/components/StatisticsPage/index.js
+++ src/components/StatisticsPage/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import Layout from '../Layout';
+import React from 'react'
+import Layout from '../Layout'
+import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Bar } from 'react-chartjs-2'
 
 export default function StatisticsPage() {
-	return (
-		<Layout>
-			<div>Hello</div>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <div className='flex justify-center mt-10'>
+        <ChakraProvider>
+          <CircularProgress value={33} size='125px' color='green.400'>
+            <CircularProgressLabel>33%</CircularProgressLabel>
+          </CircularProgress>
+        </ChakraProvider>
+      </div>
+      </Layout>
+  )
 }
