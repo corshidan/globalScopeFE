2e70a3f840edc6cf84926bfc721a26cbfb490f71
proencaf <83221947+proencaf@users.noreply.github.com>
2021-11-01 17:28:07 +0000

    stats graph

--- src/components/StatisticsPage/index.js
+++ src/components/StatisticsPage/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../Layout'
 import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
 import { ChakraProvider } from '@chakra-ui/react'
-import { Pie } from 'react-chartjs-2'
+import { Line } from 'react-chartjs-2'
 
 export default function StatisticsPage() {
   // const findDate = () => {
@@ -25,36 +25,15 @@ export default function StatisticsPage() {
         </ChakraProvider>
       </div>
       <div>
-        <Pie
+        <Line
           data={{
-            labels: [
-              'Crying',
-              'Sad',
-              'Neutral',
-              'Confused',
-              'Happy',
-              'Star Struck',
-            ],
+            labels: ['😭', '🙁', '😐', '🤔', '😀', '🤩 '],
             datasets: [
               {
                 label: '# of votes',
-                data: [10, 12, 15, 4, 2],
-                backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)',
-                ],
-                borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)',
-                ],
+                data: [10, 12, 15, 4, 2, 1],
+                backgroundColor: ['rgba(153, 102, 255, 1)'],
+                borderColor: ['rgba(153, 102, 255, 1)'],
                 borderWidth: 3,
               },
               // {
