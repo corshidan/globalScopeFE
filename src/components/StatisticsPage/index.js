bd6088b5f953b6fb9a0490d7a990e54abbf8861e
proencaf <83221947+proencaf@users.noreply.github.com>
2021-11-01 22:17:31 +0000

    added another graph, no styling yet

--- src/components/StatisticsPage/index.js
+++ src/components/StatisticsPage/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../Layout'
 import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
 import { ChakraProvider } from '@chakra-ui/react'
-import { Line } from 'react-chartjs-2'
+import { Line, Bar } from 'react-chartjs-2'
 
 export default function StatisticsPage() {
   // const findDate = () => {
@@ -17,54 +17,107 @@ export default function StatisticsPage() {
   // }
   return (
     <Layout>
-      <div className='flex justify-center mt-10'>
+      {/* <div
+        className='flex flex-col bg-contain mb-3 w-full h-full bg-no-repeat bg-top pr-4 pl-4 pt-3 opacity-40 '
+        style={{ backgroundImage: 'url(/images/seeYourStatsImage.png)' }}
+      > */}
+      <div className='flex justify-center pt-6 pb-10 '>
         <ChakraProvider>
           <CircularProgress value={85} size='125px' color='green.400'>
             <CircularProgressLabel>33%</CircularProgressLabel>
           </CircularProgress>
         </ChakraProvider>
       </div>
-      <div>
-        <Line
-          data={{
-            labels: ['😭', '🙁', '😐', '🤔', '😀', '🤩 '],
-            datasets: [
-              {
-                label: '# of votes',
-                data: [10, 12, 15, 4, 2, 1],
-                backgroundColor: ['rgba(153, 102, 255, 1)'],
-                borderColor: ['rgba(153, 102, 255, 1)'],
-                borderWidth: 3,
-              },
-              // {
-              //   label: 'Quantity',
-              //   data: [47, 52, 67, 58, 9, 50],
-              //   backgroundColor: 'orange',
-              //   borderColor: 'red',
-              // },
-            ],
-          }}
-          height={400}
-          width={600}
-          options={{
-            maintainAspectRatio: false,
-            scales: {
-              yAxes: [
+      <div className='p-10 grid grid-cols-2 gap-10  '>
+        <div className=''>
+          <Line
+            data={{
+              labels: ['😭', '🙁', '😐', '🤔', '😀', '🤩 '],
+              datasets: [
                 {
-                  ticks: {
-                    beginAtZero: true,
-                  },
+                  label: '# of votes',
+                  // fill: true,
+                  data: [10, 12, 15, 4, 2, 1],
+                  backgroundColor: ['green'],
+                  borderColor: ['rgba(153, 102, 255, 1)'],
+                  borderWidth: 3,
                 },
+                // {
+                //   label: 'Quantity',
+                //   data: [47, 52, 67, 58, 9, 50],
+                //   backgroundColor: 'orange',
+                //   borderColor: 'red',
+                // },
               ],
-            },
-            legend: {
-              labels: {
-                fontSize: 25,
+            }}
+            height={400}
+            width={600}
+            options={{
+              maintainAspectRatio: false,
+              scales: {
+                yAxes: [
+                  {
+                    ticks: {
+                      beginAtZero: true,
+                    },
+                  },
+                ],
               },
-            },
-          }}
-        />
+              legend: {
+                labels: {
+                  fontSize: 25,
+                },
+              },
+            }}
+          />
+        </div>
+        <div className=''>
+          <Bar
+            data={{
+              labels: ['😭', '🙁', '😐', '🤔', '😀', '🤩 '],
+              datasets: [
+                {
+                  label: '# of votes',
+                  data: [10, 12, 15, 4, 2, 1],
+                  backgroundColor: ['rgba(153, 102, 255, 1)'],
+                  borderColor: ['rgba(153, 102, 255, 1)'],
+                  borderWidth: 3,
+                  // fill: true,
+                },
+                // {
+                //   label: 'Quantity',
+                //   data: [47, 52, 67, 58, 9, 50],
+                //   backgroundColor: 'orange',
+                //   borderColor: 'red',
+                // },
+              ],
+            }}
+            height={400}
+            width={600}
+            options={{
+              maintainAspectRatio: false,
+              scales: {
+                yAxes: [
+                  {
+                    ticks: {
+                      beginAtZero: true,
+                      // type: 'linear',
+                      // display: false,
+                      offset: true,
+                    },
+                  },
+                ],
+              },
+              legend: {
+                labels: {
+                  fontSize: 25,
+                },
+              },
+            }}
+          />
+        </div>
       </div>
+      {/* </div> */}
     </Layout>
   )
 }
