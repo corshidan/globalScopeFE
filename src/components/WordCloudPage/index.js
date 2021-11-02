2e49b24c6fc277877e47ab83da23abc45e6306cd
corshi.dan <corshi.dan@gmail.com>
2021-11-02 11:20:32 +0000

    word cloud bg and resize

--- src/components/WordCloudPage/index.js
+++ src/components/WordCloudPage/index.js
@@ -4,7 +4,14 @@ import Wordcloud from '../WordCloud';
 export default function WordCloudPage() {
 	return (
 		<Layout>
-			<Wordcloud />
+			<div
+				className="bg-contain bg-no-repeat bg-top h-full"
+				style={{ backgroundImage: 'url(/images/lettersBro.png)' }}
+			>
+				<div className="bg-white opacity-90 h-full">
+					<Wordcloud />
+				</div>
+			</div>
 		</Layout>
 	);
 }
