4d27353fae490990d53d65a84fb69184ea871789
Waseem <waseembenaras@gmail.com>
2021-11-03 16:35:47 +0000

    more changes to stats

--- src/components/StatisticsPage/index.js
+++ src/components/StatisticsPage/index.js
@@ -7,8 +7,8 @@ import { useUser } from '../App/App.js'
 import GaugeChart from 'react-gauge-chart'
 
 export default function StatisticsPage() {
-  const [confidence, setConfidence] = useState()
-  const [feelings, setFeelings] = useState()
+  const [confidence, setConfidence] = useState(1)
+  const [feelings, setFeelings] = useState(1)
   const user = useUser()
   useEffect(() => {
     fetch(`https://global-scope.herokuapp.com/reflections/${user.bootcamperid}`)
@@ -27,7 +27,7 @@ export default function StatisticsPage() {
 
         const emojiReducer = feeling.reduce((a, c) => a + c)
         const averageFeeling = emojiReducer / feeling.length
-
+console.log(averageFeeling)
         setFeelings(averageFeeling)
 
         const reducer = overallConfidence.reduce((a, c) => a + c)
@@ -54,13 +54,13 @@ export default function StatisticsPage() {
   }
   let value = findDate()
   let progressPercentage = findDate() / 100
-  let confidencePercentage = Math.round((Math.round(confidence) * 2 - 1) / 10)
-  console.log(typeof confidence)
-  console.log(confidencePercentage)
+  
+  
   return (
     <Layout>
-      <div className='grid grid-cols-3'>
-        <div className='flex flex-col self-center items-center pt-6 pb-10 bg-gray-100 w-2/4 rounded-b-xl  mx-auto mt-4 shadow-inner '>
+    <div className="py-4 px-2">
+      <div className='grid grid-cols-3 px-10 gap-10'>
+        <div className='flex flex-col  items-center mt-5 pt-6 pb-10 bg-gray-100  h-3/5 rounded-b-xl   mt-4 shadow-inner '>
           {/* <ChakraProvider>
             <CircularProgress value={findDate()} size='125px' color='#401485'>
               <CircularProgressLabel> {value}%</CircularProgressLabel>
@@ -80,7 +80,7 @@ export default function StatisticsPage() {
           />
           <p>{value}%</p>
         </div>
-        <div className='flex flex-col justify-center items-center pt-6 pb-10 bg-gray-100 w-2/4 rounded-b-xl  mx-auto mt-4 shadow-inner '>
+        <div className='flex flex-col justify-center items-center pt-6 pb-10 bg-gray-100 w-3/4 h-3/4 rounded-b-xl  mx-auto mt-4 shadow-inner '>
           {/* <ChakraProvider>
             <CircularProgress value={100} size='125px' color='green'>
               <CircularProgressLabel> 5</CircularProgressLabel>
@@ -90,7 +90,7 @@ export default function StatisticsPage() {
             id='gauge-chart2'
             nrOfLevels={6}
             arcWidth={0.3}
-            percent={1}
+            percent={(Math.round(feelings) * 1.67 - 1) / 10}
             colors={['rgba(255, 206, 86, 0.7)', 'rgba(255, 180, 0, 0.9)']}
             style={{ width: '120%' }}
             needleBaseColor={'#401485'}
@@ -104,7 +104,7 @@ export default function StatisticsPage() {
             alt='emoji face'
           />
         </div>
-        <div className='flex flex-col justify-center items-center pt-6 pb-10 bg-gray-100 w-2/4 rounded-b-xl  mx-auto mt-4 shadow-inner '>
+        <div className='flex flex-col justify-center justify-self-end items-center  pt-6 pb-10 bg-gray-100 w-3/4 h-3/4 rounded-b-xl  mt-4 shadow-inner '>
           {/* <ChakraProvider>
             <CircularProgress value={25} size='125px' color='red'>
               <CircularProgressLabel> 1.8%</CircularProgressLabel>
@@ -112,7 +112,7 @@ export default function StatisticsPage() {
           </ChakraProvider> */}
           <GaugeChart
             id='gauge-chart1'
-            percent={confidencePercentage}
+            percent={(Math.round(confidence) * 2 - 1) / 10}
             nrOfLevels={5}
             arcWidth={0.3}
             style={{ width: '120%' }}
@@ -125,11 +125,11 @@ export default function StatisticsPage() {
               'rgba(20, 220, 20, 0.7)',
             ]}
           />
-          <p>{confidence}</p>
+          <p>{Math.round(confidence)}</p>
         </div>
       </div>
 
-      <div className='p-10 grid grid-cols-2 gap-10   '>
+      <div className='px-10 grid grid-cols-2 gap-10   '>
         <div className=' bg-gray-100 rounded-l-xl  drop-shadow-2xl  shadow-inner'>
           <PolarArea
             data={{
@@ -138,7 +138,7 @@ export default function StatisticsPage() {
                 {
                   label: 'Overall mood',
                   fill: true,
-                  data: [10, 12, 15, 4, 2, 1],
+                  data: [10, 1, 10, 4, 2, 1],
                   backgroundColor: [
                     'rgba(255, 99, 132, 0.5)',
                     'rgba(54, 162, 235, 0.5)',
@@ -181,7 +181,9 @@ export default function StatisticsPage() {
         </div>
         <div className=' bg-gray-100 rounded-r-xl z-10 shadow-inner'>
           <Bar
+          
             data={{
+              
               labels: ['1', '2', '3', '4', '5'],
               datasets: [
                 {
@@ -232,6 +234,7 @@ export default function StatisticsPage() {
           />
         </div>
       </div>
+    </div>
     </Layout>
   )
 }
